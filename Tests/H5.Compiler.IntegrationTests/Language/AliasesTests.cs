64b4e8f092313eebcff8012d3328c1a940db2e5e
theolivenbaum <rafael@curiosity.ai>
2026-02-16 14:48:52 +0100

    Update AliasesTests.cs

--- Tests/H5.Compiler.IntegrationTests/Language/AliasesTests.cs
+++ Tests/H5.Compiler.IntegrationTests/Language/AliasesTests.cs
@@ -19,30 +19,36 @@ namespace A
   public class Test { public string Name => "A"; }
 }
 
-namespace  B
+namespace B
 {
   public class Test { public string Name => "B"; }
 }
 
 namespace C
 {
-   using Test = B.Test;
+   using A;
+   using B;
+   using Test = A.Test;
 
    public class Program
    {
        public static void Main()
        {
-           var t = GetTest();
-           Console.WriteLine(t.Name);
+           Console.WriteLine(GetTestA().Name);
+           Console.WriteLine(GetTestB().Name);
+           Console.WriteLine(GetTest().Name);
        }
 
+       public static A.Test GetTestA() => new A.Test();
+       public static B.Test GetTestB() => new B.Test();
        public static Test GetTest() => new Test();
+
    }
 }
 """;
             // The output should be "B" because GetTest() returns Test which is aliased to B.Test
             var output = await RunTest(code, skipRoslyn: true);
-            Assert.AreEqual("B", output.Trim());
+            Assert.AreEqual("A\nB\nA", output.Trim());
         }
     }
 }
