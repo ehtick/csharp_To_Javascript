8c3fe94c0469893542a7c9bf8b2171537a80a46c
theolivenbaum <rafael@curiosity.ai>
2026-02-16 21:00:57 +0100

    Move test

--- Tests/H5.Compiler.IntegrationTests/Language/CSharp13Tests.cs
+++ Tests/H5.Compiler.IntegrationTests/Language/CSharp13Tests.cs
@@ -32,6 +32,36 @@ public class Program
         }
 
         [TestMethod]
+        public async Task ParamsCollections2()
+        {
+            var code = """
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        PrintList(1, 2, 3);
+        PrintSpan(4, 5, 6);
+    }
+
+    static void PrintList(params List<int> list)
+    {
+        Console.WriteLine(list.Count);
+    }
+
+    static void PrintSpan(params ReadOnlySpan<int> span)
+    {
+        Console.WriteLine(span.Length);
+    }
+}
+""";
+            await RunTest(code);
+        }
+
+
+        [TestMethod]
         public async Task LockObject()
         {
             var code = """
