6e68c44441f8ac86c15741d081277bfb908c21a5
theolivenbaum <rafael@curiosity.ai>
2026-02-16 21:07:41 +0100

    Update MemoryExtensions.cs

--- H5/H5/System/MemoryExtensions.cs
+++ H5/H5/System/MemoryExtensions.cs
@@ -32,18 +32,6 @@ namespace System
                  return default;
              }
 
-             // In case ToCharArray(start, length) is not available/reliable in all H5 targets,
-             // using Substring + ToCharArray is safer given we are in H5 environment where string ops are cheapish (JS strings).
-             // But actually ToCharArray() creates an array, so Substring is fine.
-             // Wait, if I use Substring, I allocate a string then an array.
-             // If I use ToCharArray(), I allocate an array.
-             // I'll assume ToCharArray() is good or just use full array + offset.
-
-             // H5 ReadOnlySpan implementation takes (array, offset, length).
-             // If I do text.ToCharArray(), I get the FULL array.
-             // Then I pass start and length to ReadOnlySpan ctor.
-             // This avoids allocating a substring.
-
              return new ReadOnlySpan<char>(text.ToCharArray(), start, length);
         }
 
